fbecc837464943d23e89e485ac4fff76ab42b1e5
annechh <142426482+annechh@users.noreply.github.com>
2024-05-01 19:05:33 +0200

    Adjusting fetch

--- JS/fetch.mjs
+++ JS/fetch.mjs
@@ -1,21 +1,24 @@
 
 const apiUrl = "https://v2.api.noroff.dev/blog/posts/Shira";
-const apiAdmin = `${apiUrl}/`
+// const apiAdmin = `${apiUrl}/`
 
-export async function fetchApi(method) {  
+export async function fetchApi(method, body) {  
     try {
+        // const token = '';
         const response = await fetch(apiUrl, {
             method: method,
             header: {
                 'Content-Type': 'application/json',
                 //Authorization: 'Bearer ${TOKEN HERE}',
             },
-            body: JSON.stringify(),
+            body: JSON.stringify(body),
         });
         
         const data = await response.json();
+        // localStorage.setItem('newPost', JSON.stringify(data));
         console.log('RESPONSE',response);
         console.log('DATA',data);
+        // window.location.href = '../post/index.html';
     } 
     catch (error) {
         console.error('Could not fetch data' + error);
