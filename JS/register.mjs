f13c59ecdd5be8ddce685fa270f7b4721c11d221
annechh <142426482+annechh@users.noreply.github.com>
2024-05-02 00:52:55 +0200

    Update register.js
    added required to input

--- JS/register.mjs
+++ JS/register.mjs
@@ -3,7 +3,7 @@ function createRegisterForm() {
     let section = document.getElementById('registerSection');
 
     let h1 = document.createElement('h1');
-    h1.innerText = 'Register User';
+    h1.innerHTML = 'Register User';
 
     let form = document.createElement('form');
     form.id = 'registerForm';
@@ -11,16 +11,41 @@ function createRegisterForm() {
     let inputName = ['Name', 'Email', 'Password', 'Confirm Password']; 
     let inputId = ['registerName', 'registerEmail', 'registerPassword', 'registerConfirmPassword'];
     let inputType = ['text', 'text', 'password', 'password'];
+    let passwordInput;
     
     for (let i = 0; i < inputName.length; i++) {
         let input = document.createElement('input');
-        input.name = inputType[i];
-        input.type = inputName[i];
+        input.name = inputName[i];
+        input.type = inputType[i];
         input.id = inputId[i];
         input.placeholder = inputName[i];
+        input.required = true;
+
+        if ( inputType[i] === 'text' && inputName[i] === 'Name') {
+            input.minLength = 2;
+        } else if (inputType[i] === 'text' && inputName[i] === 'Email') {
+            input.pattern = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$';
+            input.title = 'Enter a valid email address'
+        } else if (inputType[i] === 'password') {
+            input.pattern = '(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$';
+            input.title = 'Password must contain at least one letter and one number';
+
+            passwordInput = input; // Store a reference to the password input element
+        } 
         
         form.appendChild(input);
     }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        let confirmPasswordInput = document.getElementById('registerConfirmPassword');
+            confirmPasswordInput.addEventListener('input', function() {
+        let password = passwordInput.value;
+        let confirmPassword = confirmPasswordInput.value;
+            if (password !== confirmPassword) {
+                alert('Passwords do not match!');
+            }
+        })
+    })
 
     let btnContainer = document.createElement('div');
     btnContainer.classList = 'btn-container';
@@ -29,6 +54,9 @@ function createRegisterForm() {
     registerButton.type = 'submit';
     registerButton.innerText = 'Register User';
     registerButton.classList = 'btn';
+    registerButton.addEventListener('click', () => {
+
+    })
 
     let changeToLogin = document.createElement('a');
     changeToLogin.innerText = 'Already a user? Log in here';
@@ -43,8 +71,23 @@ function createRegisterForm() {
 createRegisterForm()
 
 
+function registerUser() {
+    
+}
+
 
 
 
 
 console.log('hello');
+
+// const eventConfirmPassword = () => {
+//     let confirmPasswordInput = document.getElementById('registerConfirmPassword');
+//     confirmPasswordInput.addEventListener('input', function() {
+//     let password = passwordInput.value;
+//     let confirmPassword = confirmPasswordInput.value;
+//     if (password !== confirmPassword) {
+//         alert('Passwords do not match!');
+//     }
+//     })
+// }
