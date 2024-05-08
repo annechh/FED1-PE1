b96bade2b71b7109a12de17f398daeae4aea9091
annechh <142426482+annechh@users.noreply.github.com>
2024-05-08 08:27:52 +0200

    Minor changes

--- JS/login.mjs
+++ JS/login.mjs
@@ -1,3 +1,5 @@
+import { loginUrl, fetchApi } from "./fetch.mjs";
+
 function createLoginForm() {
     let section = document.getElementById('loginSection');
 
@@ -6,37 +8,121 @@ function createLoginForm() {
 
     let form = document.createElement('form');
     form.id = 'loginForm';
-    
-    let inputName = ['Email', 'Password']; 
-    let inputId = ['loginEmail', 'loginPassword'];
-    let inputType = ['text', 'password'];
-    
-    for (let i = 0; i < inputName.length; i++) {
-        let input = document.createElement('input');
-        input.name = inputType[i];
-        input.type = inputName[i];
-        input.id = inputId[i];
-        input.placeholder = inputName[i];
-        
-        form.appendChild(input);
-    }
+    form.addEventListener('submit', handleLogin);
+
+    let email = document.createElement('input');
+    email.name = 'Email';
+    email.type = 'text';
+    email.id = 'loginEmail';
+    email.placeholder = 'Email';
+    email.required = true;
+
+    let password = document.createElement('input');
+    password.name = 'Password';
+    password.type = 'password';
+    password.id = 'loginPassword';
+    password.placeholder = 'Password';
+    password.required = true;
 
     let btnContainer = document.createElement('div');
     btnContainer.classList = 'btn-container';
 
-    let registerButton = document.createElement('button');
-    registerButton.type = 'submit';
-    registerButton.innerText = 'Log In';
-    registerButton.classList = 'btn';
+    let loginButton = document.createElement('button');
+    loginButton.type = 'submit';
+    loginButton.innerText = 'Log In';
+    loginButton.classList = 'btn';
+    loginButton.id = 'loginBtn'
 
     let changeToRegister = document.createElement('a');
     changeToRegister.innerText = 'Not a user? Register here';
     changeToRegister.href = '../account/register.html';
 
-    btnContainer.append(registerButton, changeToRegister)
-    section.append(h1, form, btnContainer);
+    btnContainer.append(loginButton, changeToRegister);
+    form.append(email, password, btnContainer)
+    section.append(h1, form);
 
     return section;
 }
 
 createLoginForm()
+
+
+
+async function handleLogin(event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+    try {
+        const userData = await fetchApi('POST', loginUrl, { email, password });
+        console.log('Login successful:', userData);
+        const accessToken = userData.data.accessToken;
+        if (accessToken) {
+            localStorage.setItem('accessToken', accessToken);
+            console.log('Token saved to local storage', accessToken);
+            window.location.href = '../index.html'
+        } else {
+            console.error('No token found', userData);
+        }
+        // return userData;
+    } catch (error) {
+        console.error('Login failed:', error);
+    }
+}
+
+export const token = localStorage.getItem('accessToken');
+
+
+
+
+// const loggedIn = document.getElementById('navLogin');
+
+// function handleLoggedIn() {
+
+//     if (loggedIn.textContent === 'Login') {
+//         loggedIn.textContent = 'Logout'
+//         loggedIn.href = '';
+
+//     } else {
+//         window.location.href = '';
+//     }
+// }
+
+// loggedIn.addEventListener('click', handleLoggedIn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Kansje bruk kansje ikke
+// async function fetchWithToken(method, url) {
+//     try {
+//         const headers = {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${token}`,
+//         };
+//         if (method === 'GET') {
+//             const response = await fetch(method, url, {headers})
+//             console.log('something',response);
+            
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+
+// fetchWithToken(loginUrl)
