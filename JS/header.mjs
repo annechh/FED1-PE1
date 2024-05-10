676c804c50988504c5d551d5797c453c23e21d85
annechh <142426482+annechh@users.noreply.github.com>
2024-05-10 16:07:37 +0200

    Path test

--- JS/header.mjs
+++ JS/header.mjs
@@ -25,10 +25,11 @@ export function createHeader() {
     let home = document.createElement('a');
         home.className = 'ds-txt hover-scale';
         home.textContent = 'Home';
-        home.href = '../index.html';
         if (window.location.pathname.endsWith === '/index.html') {
             home.href = '#';
-        } 
+        } else {
+            home.href = '../index.html';
+        }
 
     let homeIcon = document.createElement('i');
         homeIcon.className = 'fa-solid fa-house';
