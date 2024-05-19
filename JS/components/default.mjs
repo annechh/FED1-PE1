ae3a7b8109f85752afbc6b46e021396ca887314d
annechh <142426482+annechh@users.noreply.github.com>
2024-05-20 01:12:57 +0200

    Moved loggedInEvents to another file

--- JS/components/default.mjs
+++ JS/components/default.mjs
@@ -11,32 +11,4 @@ export function fontawsomeScript() {
 fontawsomeScript()
 
 
-// export function loggedInEvents() {
-//     const userDataString = localStorage.getItem('userData');
-//     const userData = JSON.parse(userDataString);
 
-//     document.addEventListener('DOMContentLoaded', () => {
-//         const loggedIn = document.querySelector('.loggedIn-state');
-//         const loggedOut = document.querySelector('.loggedOut-state');
-//         // const newPostButton = document.getElementById('newPost');
-
-        
-        
-//         if (userData) {
-//             // console.log('User is logged in', userData);
-//             loggedOut.style.display = 'flex'
-//             loggedIn.style.display = 'none'
-//             // newPostButton.style.display = 'block';
-
-                        
-//         } else {
-//             console.log('No user logged in');
-//             loggedIn.style.display = 'flex';
-//             loggedOut.style.display = 'none';
-//             // newPostButton.style.display = 'none'
-
-//         }
-//     }) 
-// } 
-
-// loggedInEvents();
