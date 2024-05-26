1ff522bbe1e807fc5626bcd76ec7c77b671c0cc4
annechh <142426482+annechh@users.noreply.github.com>
2024-05-27 00:05:43 +0200

    Trying another fetch

--- JS/fetch.mjs
+++ JS/fetch.mjs
@@ -7,8 +7,8 @@ export const registerUrl = `${baseUrl}/auth/register`;
 
 
 
-export async function fetchApi(method, url, body) {  
-// const token = localStorage.getItem('accessToken');
+export async function fetchApi(method, url, body = null) {  
+const token = localStorage.getItem('accessToken');
     
     showLoader();
     try {
@@ -16,26 +16,56 @@ export async function fetchApi(method, url, body) {
             method: method,
             headers: {
                 'Content-Type': 'application/json',
-                // Authorization: `Bearer ${token}`,
-                
+                Authorization: `Bearer ${token}`,    
             },
-            body: JSON.stringify(body),
+            body: body ? JSON.stringify(body) : null,
         });
         
-        const data = await response.json();
-        console.log('DATA',data, 'RESPONSE', response);
-        // console.log('RESPONSE',response);
-        return data;
-    } 
-    catch (error) {
-        console.error('Could not fetch data' + error);
-        throw error ("There was a problem getting the data")
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const contentType = response.headers.get('Content-Type');
+        if (contentType && contentType.includes('application/json')) {
+            console.log('Content type',contentType);
+            return await response.json();
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error('Could not fetch data:', error);
+        throw new Error("There was a problem getting the data");
     } finally {
         hideLoader();
     }
 }
 
-
+// export async function fetchApi(method, url, body) {  
+//     const token = localStorage.getItem('accessToken');
+    
+//     showLoader();
+//     try {
+//         const response = await fetch(url, {
+//             method: method,
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 Authorization: `Bearer ${token}`,
+                
+//             },
+//             body: JSON.stringify(body),
+//         });
+        
+//         const data = await response.json();
+//         console.log('DATA',data, 'RESPONSE', response);
+//         // console.log('RESPONSE',response);
+//         return data;
+//     } 
+//     catch (error) {
+//         console.error('Could not fetch data' + error);
+//         throw error ("There was a problem getting the data")
+//     } finally {
+//         hideLoader();
+//     }
+// }
 
 function showLoader() {
     const loader = document.getElementById('loader');
