feec9ba8136f61ec30340b0404fb8ee6d950fab1
annechh <142426482+annechh@users.noreply.github.com>
2024-05-26 17:50:41 +0200

    Added loader

--- JS/fetch.mjs
+++ JS/fetch.mjs
@@ -9,8 +9,8 @@ export const registerUrl = `${baseUrl}/auth/register`;
 
 export async function fetchApi(method, url, body) {  
     
+    showLoader();
     try {
-        // const token = '';
         const response = await fetch(url, {
             method: method,
             headers: {
@@ -22,13 +22,31 @@ export async function fetchApi(method, url, body) {
         });
         
         const data = await response.json();
+        console.log('DATA',data, 'RESPONSE', response);
         // console.log('RESPONSE',response);
-        // console.log('DATA',data);
         return data;
     } 
     catch (error) {
         console.error('Could not fetch data' + error);
         throw error ("There was a problem getting the data")
+    } finally {
+        // hideLoader();
+    }
+}
+
+
+
+function showLoader() {
+    const loader = document.getElementById('loader');
+    if (loader) {
+        loader.style.display = 'block';
+    }
+}
+
+function hideLoader() {
+    const loader = document.getElementById('loader');
+    if (loader) {
+        loader.style.display = 'none';
     }
 }
 
@@ -40,101 +58,3 @@ export async function fetchApi(method, url, body) {
 // GET, POST - /blog/posts/Shira
 // PUT, DELETE, GET - /blog/posts/Shira/{id}
 // https://v2.api.noroff.dev/blog/posts/Shira
-
-
-// from login.mjs file
-// import { loginUrl, fetchApi } from "./fetch.mjs";
-
-// function createLoginForm() {
-//     let section = document.getElementById('loginSection');
-
-//     let h1 = document.createElement('h1');
-//     h1.innerText = 'Log In';
-
-//     let form = document.createElement('form');
-//     form.id = 'loginForm';
-//     // form.addEventListener('submit', handleLogin);
-
-//     let inputName = ['Email', 'Password'];
-//     let inputId = ['loginEmail', 'loginPassword'];
-//     let inputType = ['text', 'password'];
-
-//     for (let i = 0; i < inputName.length; i++) {
-//         let input = document.createElement('input');
-//         input.name = inputName[i];
-//         input.type = inputType[i];
-//         input.id = inputId[i];
-//         input.placeholder = inputName[i];
-
-//         form.appendChild(input);
-//     }
-
-//     let btnContainer = document.createElement('div');
-//     btnContainer.classList = 'btn-container';
-
-//     let loginButton = document.createElement('button');
-//     loginButton.type = 'submit';
-//     loginButton.innerText = 'Log In';
-//     loginButton.classList = 'btn';
-//     loginButton.id = 'loginBtn'
-
-//     let changeToRegister = document.createElement('a');
-//     changeToRegister.innerText = 'Not a user? Register here';
-//     changeToRegister.href = '../account/register.html';
-
-//     btnContainer.append(loginButton, changeToRegister);
-
-//     form.appendChild(btnContainer)
-//     section.append(h1, form);
-
-//     form.addEventListener('submit', handleLoginFormSubmit);
-
-//     return section;
-// }
-
-// createLoginForm()
-
-
-
-
-
-
-// function handleLoginFormSubmit(event) {
-//     event.preventDefault();
-
-//     const email = document.getElementById('loginEmail').value;
-//     const password = document.getElementById('loginPassword').value;
-
-//     handleLogin(email, password);
-// }
-
-// async function handleLogin(event) {
-//     event.preventDefault(); 
-
-//     // const email = document.getElementById('loginEmail').value;
-//     // const password = document.getElementById('loginPassword').value;
-
-//     try {
-//         const userData = await fetchApi('POST', loginUrl, { email, password });
-//         console.log('Login successful:', userData);
-
-//         const accessToken = userData.data.accessToken;
-//         if (accessToken) {
-//             localStorage.setItem('accessToken', accessToken);
-//             console.log('Token saved to local storage', accessToken);
-
-//             window.location.href = '../index.html'
-//         } else {
-//             console.error('No token found', userData);
-//         }
-
-//         // return userData;
-
-//     } catch (error) {
-//         console.error('Login failed:', error);
-//     }
-// }
-
-// // const token = localStorage.getItem('accessToken');
-
-
