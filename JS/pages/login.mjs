e3cc21182061fc8ced655940973e5f7d77b85e42
annechh <142426482+annechh@users.noreply.github.com>
2024-05-27 01:09:02 +0200

    Changed the handle login and added validations

--- JS/pages/login.mjs
+++ JS/pages/login.mjs
@@ -2,58 +2,84 @@ import { fontawsomeScript } from "../components/default.mjs";
 import { createHeader } from "../components/header.mjs"; 
 import { loggedInEvents } from "../components/loginState.mjs";
 import { fetchApi, loginUrl } from "../fetch.mjs";
+import { checkIfEmptyField, emailValidation, passwordValidation } from "../components/validation.mjs";
 
+
+function runLoginPage() {
+    hideLoginAlert();
+    createLoginForm();
+}
 
 
 function createLoginForm() {
-    let section = document.getElementById('loginSection');
+    const section = document.getElementById('loginSection');
 
-    let h1 = document.createElement('h1');
+    const h1 = document.createElement('h1');
     h1.innerText = 'Log In';
 
-    let form = document.createElement('form');
-    form.id = 'loginForm';
-    form.addEventListener('submit', handleLogin, loggedInEvents());
+    const form = document.createElement('form');
+        form.id = 'loginForm';
+        form.addEventListener('submit', loggedInEvents());
 
-    let email = document.createElement('input');
-    email.name = 'Email';
-    email.type = 'text';
-    email.id = 'loginEmail';
-    email.placeholder = 'Email';
-    email.required = true;
+    const emailDiv = document.createElement('div');
+        emailDiv.classList.add('form-div')
 
-    let password = document.createElement('input');
-    password.name = 'Password';
-    password.type = 'password';
-    password.id = 'loginPassword';
-    password.placeholder = 'Password';
-    password.required = true;
+        const email = document.createElement('input');
+            email.name = 'Email';
+            email.type = 'text';
+            email.id = 'loginEmail';
+            email.placeholder = 'Email';
+            email.required = true;
 
-    let btnContainer = document.createElement('div');
-    btnContainer.classList = 'btn-container';
+        const emailError = document.createElement('label');
+            emailError.id = 'emailError';
+            emailError.classList.add('error-message', 'hide', 'pl');    
 
-    let loginButton = document.createElement('button');
-    loginButton.type = 'submit';
-    loginButton.innerText = 'Log In';
-    loginButton.classList = 'btn';
-    loginButton.id = 'loginBtn'
+    const passwordDiv = document.createElement('div');
+            passwordDiv.classList.add('form-div')
 
-    let changeToRegister = document.createElement('a');
+        const password = document.createElement('input');
+            password.name = 'Password';
+            password.type = 'password';
+            password.id = 'loginPassword';
+            password.placeholder = 'Password';
+            password.required = true;
+
+        const passwordError = document.createElement('label');
+            passwordError.id = 'passwordError';
+            passwordError.classList.add('error-message', 'hide', 'pl');
+
+    const btnContainer = document.createElement('div');
+        btnContainer.classList = 'btn-container';
+
+    const loginButton = document.createElement('button');
+        loginButton.type = 'submit';
+        loginButton.innerText = 'Log In';
+        loginButton.classList = 'btn';
+        loginButton.id = 'loginBtn';
+        loginButton.addEventListener('click', (event) => {
+            event.preventDefault(); 
+            handleLogin(); 
+        });
+
+    const changeToRegister = document.createElement('a');
     changeToRegister.innerText = 'Not a user? Register here';
     changeToRegister.href = '../account/register.html';
 
     btnContainer.append(loginButton, changeToRegister);
-    form.append(email, password, btnContainer)
+    passwordDiv.append(password, passwordError);
+    emailDiv.append(email, emailError);
+    form.append(emailDiv, passwordDiv, btnContainer)
     section.append(h1, form);
 
     return section;
 }
 
-createLoginForm()
 
-async function handleLogin(event) {
-    event.preventDefault(); 
-    
+async function fetchData() {    
+    validateEmail() 
+    validatePassword() 
+
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginPassword').value;
     const userData = await fetchApi('POST', loginUrl, { email, password });
@@ -76,6 +102,35 @@ async function handleLogin(event) {
 }
 
 
+async function handleLogin() {
+    validateEmail();
+    validatePassword();
+
+    const emailError = document.getElementById('emailError').textContent;
+    const passwordError = document.getElementById('passwordError').textContent;
+
+    if (emailError || passwordError) {
+        return;
+    }
+    fetchData()
+}
+
+
+function validateEmail() {
+    const email = document.getElementById('loginEmail').value;
+    const emailError = document.getElementById('emailError');
+    if (checkIfEmptyField(email, emailError)) return;
+    emailValidation(email, emailError);
+}
+
+function validatePassword() {
+    const password = document.getElementById('loginPassword').value;
+    const passwordError = document.getElementById('passwordError');
+    if (checkIfEmptyField(password, passwordError)) return;
+    passwordValidation(password, passwordError); 
+}
+
+
 function showLoginAlert() {
     const loginAlert = document.getElementById('loginAlert');
     const logInForm = document.getElementById('loginSection');
@@ -93,4 +148,6 @@ function hideLoginAlert() {
         loginAlert.style.display = 'none';
     }
 }
-hideLoginAlert()
+
+
+runLoginPage()
