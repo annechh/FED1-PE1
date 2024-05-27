43c50b42554c52785859ea0e22604425e937fef7
annechh <142426482+annechh@users.noreply.github.com>
2024-05-27 21:59:50 +0200

    Removed comments

--- JS/components/fetch.mjs
+++ JS/components/fetch.mjs
@@ -7,7 +7,7 @@ export const registerUrl = `${baseUrl}/auth/register`;
 
 export async function fetchApi(method, url, body = null) {  
 const token = localStorage.getItem('accessToken');
-    
+
     showLoader();
     try {
         const response = await fetch(url, {
@@ -51,40 +51,3 @@ function hideLoader() {
         loader.style.display = 'none';
     }
 }
-
-
-// Make userUrl usable for whatever registered account that is being made
-// `${baseUrl}/blog/posts/${name}`
-
-// GET, POST - /blog/posts/Shira
-// PUT, DELETE, GET - /blog/posts/Shira/{id}
-// https://v2.api.noroff.dev/blog/posts/Shira
-
-
-// export async function fetchApi(method, url, body) {  
-//     const token = localStorage.getItem('accessToken');
-    
-//     showLoader();
-//     try {
-//         const response = await fetch(url, {
-//             method: method,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 Authorization: `Bearer ${token}`,
-                
-//             },
-//             body: JSON.stringify(body),
-//         });
-        
-//         const data = await response.json();
-//         console.log('DATA',data, 'RESPONSE', response);
-//         // console.log('RESPONSE',response);
-//         return data;
-//     } 
-//     catch (error) {
-//         console.error('Could not fetch data' + error);
-//         throw error ("There was a problem getting the data")
-//     } finally {
-//         hideLoader();
-//     }
-// }
